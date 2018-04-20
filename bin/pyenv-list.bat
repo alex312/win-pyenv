223cd922b4fddc156e5776a8ea7797382d29fd1c
alex312 <xuan.li@bonintech.com>
2018-04-20 18:03:10 +0800

    fix bug: python list envs 返回结果是系统中的python版本号列表

--- bin/pyenv-list.bat
+++ bin/pyenv-list.bat
@@ -33,7 +33,7 @@ for /f "tokens=1,2 delims==" %%i in ('call %UTILITS_PATH%\versions_get system') 
 :show_env
 if "%list_type%"=="system" goto:eof
 echo;
-for /f "tokens=1,2 delims==" %%i in ('call %UTILITS_PATH%\versions_get system') do ( echo %%i )
+for /f "tokens=1,2 delims==" %%i in ('call %UTILITS_PATH%\versions_get envs') do ( echo %%i )
 
 goto:eof
 
